4455325a6a4cac5086764c56212091fb523036d5
Slava Tsvetkov <tsvetkoffff@gmail.com>
2022-05-02 13:40:29 +0300

    feat: solve 5 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,32 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity != 'string' ||
+    isNaN(parseInt(sampleActivity, 10)) ||
+    parseInt(sampleActivity, 10) <= 0 ||
+    parseFloat(sampleActivity, 10) > 15
+  )
+    return false;
+  const result = Math.ceil(
+    Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+  );
+  return result;
   // remove line with error and write your code here
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(backyard) {
+  let count = 0
+  backyard.flat().map(e => {
+    e == '^^' ? ++count : null
+  })
+  return count
   // remove line with error and write your code here
 }
 
